72eddfb2a2ccf987bd983f5a02e2b8f6adfb2c96
= <dhilpipre@newrelic.com>
2020-12-21 15:35:01 -0600

    fixed problem with non-expired tokens that caused overhead

--- Kotlin-Coroutines_1.3.3/src/main/java/kotlin/coroutines/jvm/internal/BaseContinuationImpl.java
+++ Kotlin-Coroutines_1.3.3/src/main/java/kotlin/coroutines/jvm/internal/BaseContinuationImpl.java
@@ -1,14 +1,43 @@
 package kotlin.coroutines.jvm.internal;
 
 import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Token;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
+
+import kotlin.coroutines.Continuation;
 
 @Weave(type=MatchType.BaseClass)
 public abstract class BaseContinuationImpl {
 	
+//	@NewField
+//	private Token token = null;
+	
+	public BaseContinuationImpl(Continuation<Object> c) {
+//		if(token == null) {
+//			token = NewRelic.getAgent().getTransaction().getToken();
+//		}
+	}
+	
+	@Trace(async=true)
+	protected Object invokeSuspend(java.lang.Object obj) {
+//		if(token != null) {
+//			token.linkAndExpire();
+//			token = null;
+//		}
+		String name =  null;
+		StackTraceElement element = getStackTraceElement();
+		if(element != null) {
+			name = element.getClassName() + "." + element.getMethodName();
+		}
+		if(name != null) {
+			NewRelic.getAgent().getTracedMethod().setMetricName("Custom","Continuation",name,"invokeSuspend");
+		}
+		return Weaver.callOriginal();
+	}
 	
 	@Trace
 	public void resumeWith(Object obj) {
--- Kotlin-Coroutines_1.3/src/main/java/kotlinx/coroutines/CoroutineStart.java
+++ Kotlin-Coroutines_1.3/src/main/java/kotlinx/coroutines/CoroutineStart.java
@@ -1,5 +1,6 @@
 package kotlinx.coroutines;
 
+import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
@@ -14,15 +15,33 @@ public abstract class CoroutineStart {
 	public static final CoroutineStart LAZY = Weaver.callOriginal();
 	public static final CoroutineStart ATOMIC = Weaver.callOriginal();
 	public static final CoroutineStart UNDISPATCHED = Weaver.callOriginal();
-	  
+	
 	@Trace
 	public final <T> void invoke(Function1<? super Continuation<? super T>, ? extends Object> f, Continuation<? super T> c) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom","Coroutines","CoroutineStart","invoke","F1",getType());
 		Weaver.callOriginal();
 	}
 	
 	@Trace
 	public final <R, T> void invoke(Function2<? super R, ? super Continuation<? super T>, ? extends java.lang.Object> f, R r, Continuation<? super T> c) {
+		NewRelic.getAgent().getTracedMethod().setMetricName("Custom","Coroutines","CoroutineStart","invoke","F2",getType());
 		Weaver.callOriginal();
 	}
 	
+	
+	private String getType() {
+		if(this == DEFAULT) {
+			return "Default";
+		}
+		if(this == ATOMIC) {
+			return "Atomic";
+		}
+		if(this == UNDISPATCHED) {
+			return "Undispatched";
+		}
+		if(this == LAZY) {
+			return "Lazy";
+		}
+		return "Unknown";
+	}
 }
