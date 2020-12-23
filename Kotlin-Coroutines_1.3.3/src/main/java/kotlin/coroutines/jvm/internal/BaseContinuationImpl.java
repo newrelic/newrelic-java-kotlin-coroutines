53faad62ccf31e28c81bfb92c5bfbcee70e55c4a
= <dhilpipre@newrelic.com>
2020-12-23 07:28:13 -0600

    fixes

--- Kotlin-Coroutines_1.3.3/src/main/java/kotlin/coroutines/jvm/internal/BaseContinuationImpl.java
+++ Kotlin-Coroutines_1.3.3/src/main/java/kotlin/coroutines/jvm/internal/BaseContinuationImpl.java
@@ -1,10 +1,8 @@
 package kotlin.coroutines.jvm.internal;
 
 import com.newrelic.api.agent.NewRelic;
-import com.newrelic.api.agent.Token;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
-import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
@@ -13,21 +11,11 @@ import kotlin.coroutines.Continuation;
 @Weave(type=MatchType.BaseClass)
 public abstract class BaseContinuationImpl {
 	
-//	@NewField
-//	private Token token = null;
-	
 	public BaseContinuationImpl(Continuation<Object> c) {
-//		if(token == null) {
-//			token = NewRelic.getAgent().getTransaction().getToken();
-//		}
 	}
 	
 	@Trace(async=true)
 	protected Object invokeSuspend(java.lang.Object obj) {
-//		if(token != null) {
-//			token.linkAndExpire();
-//			token = null;
-//		}
 		String name =  null;
 		StackTraceElement element = getStackTraceElement();
 		if(element != null) {
